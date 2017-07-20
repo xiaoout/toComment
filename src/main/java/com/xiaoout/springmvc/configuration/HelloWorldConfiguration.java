ebd9dd8467de5d0134984ba4df7bb79615bfe8d6
cchen17 <clarence.chen@morningstar.com>
2017-07-20 14:36:57 +0800

    Form

--- src/main/java/com/xiaoout/springmvc/configuration/HelloWorldConfiguration.java
+++ src/main/java/com/xiaoout/springmvc/configuration/HelloWorldConfiguration.java
@@ -1,29 +1,57 @@
 package com.xiaoout.springmvc.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by cchen17 on 6/12/2017.
  */
-    @Configuration
-    @EnableWebMvc
-    @ComponentScan(basePackages = "com.xiaoout.springmvc")
-    public class HelloWorldConfiguration {
-        @Bean
-        public ViewResolver viewResolver() {
-            InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-            viewResolver.setViewClass(JstlView.class);
-            viewResolver.setPrefix("/WEB-INF/views/");
-            viewResolver.setSuffix(".jsp");
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.xiaoout.springmvc")
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
 
-            return viewResolver;
-        }
-
+    /*
+ * Configure View Resolver
+ */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
     }
+
+    /*
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     *
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+	/*
+     * Configure MessageSource to provide internationalized messages
+	 *
+	 */
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+}
 
--- src/main/java/com/xiaoout/springmvc/configuration/HelloWorldInitializer.java
+++ src/main/java/com/xiaoout/springmvc/configuration/HelloWorldInitializer.java
@@ -1,24 +1,29 @@
 package com.xiaoout.springmvc.configuration;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Created by cchen17 on 6/12/2017.
  */
-public class HelloWorldInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class HelloWorldInitializer implements WebApplicationInitializer {
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] { "/" };
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(HelloWorldConfiguration.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = container.addServlet(
+                "dispatcher", new DispatcherServlet(ctx));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
     }
 
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { HelloWorldConfiguration.class };
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return null;
-    }
 }
